96a474a466005c6be657d346cf86461533f57a42
LBibek <bibeklamatamg@gmail.com>
2019-05-27 20:35:37 +0545

    settings-updated

--- public/js/app.js
+++ public/js/app.js
@@ -13,6 +13,43 @@
        })
    }
 
+   //for item saving
+   $scope.saveItem = function(){
+    console.log($scope.item);
+    $http.post('/post/item', $scope.item).then(function(res){
+        console.log(res.data);
+        $scope.message = res.data;
+        $timeout(function(){
+        $scope.message = '';
+        }, 3000);
+       loaditem();
+    })
+}
+//item
+$scope.editItem = function(id){
+    $http.get('/get/single/item/'+id).then(function(res){
+        $scope.singleItem = res.data;
+    })
+}
+$scope.updateItem = function(id){
+    console.log($scope.singleUser);
+  $http.post('/update/item/'+id, $scope.singleItem).then(function(res){
+      console.log(res.data);
+      $scope.message = res.data;
+      $timeout(function(){
+      $scope.message = '';
+      }, 3000);
+     loaditem();
+  });
+}
+function loaditem(){
+    $http.get('/get/all/item').then(function(res){
+        $scope.items = res.data;
+        //
+    })
+  }
+
+  loaditem();
   function loaduser(){
     $http.get('/get/user').then(function(res){
         $scope.users = res.data;
@@ -27,7 +64,6 @@
   }
   $scope.updateUser = function(id){
       console.log($scope.singleUser);
-      
     $http.post('/update/user/'+id, $scope.singleUser).then(function(res){
         console.log(res.data);
         $scope.message = res.data;
@@ -37,4 +73,35 @@
        loaduser();
     });
   }
+
+//   save setting
+$scope.saveSetting = function(id){
+    console.log($scope.set);
+    if($scope.set){
+          $http.post('/post/setting/'+id, $scope.set).then(function(res){
+              $scope.message = res.data;
+              $timeout(function(){
+                $scope.message = '';
+                }, 3000);
+                window.location = '/users'
+                loadSetting()
+          })
+    }else{
+        $scope.message = "Change settings first."
+        $timeout(function(){
+            $scope.message = '';
+            }, 3000);
+            loadSetting()
+    }
+}
+
+function loadSetting(){
+    $http.get('/get/setting').then(function(res){
+        $scope.set = res.data[0];
+        console.log(res.data);
+    })
+  }
+  loadSetting();
+
+// end
  })
